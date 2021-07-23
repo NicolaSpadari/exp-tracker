8890d2888e247b4474ec2a094fde7aed75c22042
Nicola Spadari <spadari.nicola@gmail.com>
2021-07-23 17:57:26 +0200

    Fix sw skip awaiting

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,9 @@
-import { precacheAndRoute } from "workbox_precaching";
+import { precacheAndRoute } from "workbox-precaching";
 
 let self;
 
-self.addEventListener("message", (e) => {
-    if (e.data && e.data.type === "SKIP_AWAITING") {
-        self.skipWaiting();
-    }
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
 
 precacheAndRoute(self.__WB_MANIFEST);
