3162f669bf9cfdc5af07f1ca651a0f2ddbeab380
Nicola Spadari <spadari.nicola@gmail.com>
2021-07-23 20:34:36 +0200

    Remove self declaration

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 import { precacheAndRoute } from "workbox-precaching";
 
-let self;
+// let self;
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
